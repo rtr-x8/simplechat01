d3ad6d0207ce4dd4c3a30f4a94f801fe128dec15
rtr-x8 <ryu.2.24@hotmail.co.jp>
2020-12-31 22:11:54 +0900

    OGP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="友達追加して、2020年をカウントダウンしよう！">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
