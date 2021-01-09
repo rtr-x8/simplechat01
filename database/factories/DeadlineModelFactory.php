e6cecc61525208107321775ee36d2883db013f1b
rtr-x8 <ryu.2.24@hotmail.co.jp>
2021-01-09 14:17:25 +0900

    factory

--- database/factories/DeadlineModelFactory.php
+++ database/factories/DeadlineModelFactory.php
@@ -2,6 +2,11 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
+use CountDownChat\Domain\Deadline\DeadlineDescription;
+use CountDownChat\Domain\Deadline\DeadlineId;
+use CountDownChat\Domain\Deadline\DeadlineName;
+use CountDownChat\Domain\Liner\LinerId;
 use CountDownChat\Infrastructure\Deadline\Model\DeadlineModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -22,7 +27,13 @@ class DeadlineModelFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'deadline_id' => DeadlineId::new()->value(),
+            'liner_id' => LinerId::new()->value(),
+            'name' => DeadlineName::of($this->faker->streetName),
+            'description' => DeadlineDescription::of($this->faker->text(100)),
+            'is_active' => true,
+            'is_complete' => false,
+            'deadline_at' => Carbon::parse('2020/01/01 00:05:00'),
         ];
     }
 }
