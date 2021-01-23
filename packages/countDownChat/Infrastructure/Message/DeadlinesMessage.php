55354dec7908526931b12d8a808f155c42cd45fb
rtr-x8 <ryu.2.24@hotmail.co.jp>
2021-01-23 20:16:20 +0900

    見た目変更

---
+++ packages/countDownChat/Infrastructure/Message/DeadlinesMessage.php
@@ -0,0 +1,90 @@
+<?php
+
+
+namespace CountDownChat\Infrastructure\Message;
+
+
+use CountDownChat\Domain\Deadline\Deadline;
+use LINE\LINEBot\Constant\Flex\BubleContainerSize;
+use LINE\LINEBot\Constant\Flex\ComponentFontSize;
+use LINE\LINEBot\Constant\Flex\ComponentFontWeight;
+use LINE\LINEBot\Constant\Flex\ComponentLayout;
+use LINE\LINEBot\Constant\Flex\ComponentSpacing;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\CarouselContainerBuilder;
+use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
+
+class DeadlinesMessage
+{
+    /**
+     * @var Deadline[]
+     */
+    private array $deadlines;
+
+    /**
+     * DeadlinesMessageBuilder constructor.
+     * @param  array  $deadlines
+     */
+    public function __construct(array $deadlines)
+    {
+        $this->deadlines = $deadlines;
+    }
+
+    /**
+     */
+    public function build()
+    {
+        $bubbles = collect($this->deadlines)->map(function (Deadline $deadline) {
+            return $this->createDeadlineBubble($deadline);
+        })->toArray();
+        return FlexMessageBuilder::builder()
+            ->setAltText('カウントダウンのお知らせです！')
+            ->setContents(new CarouselContainerBuilder($bubbles));
+    }
+
+    /**
+     * @param  Deadline  $deadline
+     * @return ContainerBuilder
+     */
+    private function createDeadlineBubble(Deadline $deadline): ContainerBuilder
+    {
+        return BubbleContainerBuilder::builder()
+            ->setHeader($this->createDeadlineBubbleHeader($deadline))
+            ->setSize(BubleContainerSize::KILO)
+            ->setBody($this->createDeadlineBubbleBody($deadline));
+    }
+
+    private function createDeadlineBubbleHeader(Deadline $deadline)
+    {
+        $headerTextComponent = TextComponentBuilder::builder()
+            ->setText($deadline->getName()->value())
+            ->setSize(ComponentFontSize::MD)
+            ->setWeight(ComponentFontWeight::BOLD)
+            ->setWrap(true)
+            ->setFlex(0);
+        return BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::VERTICAL)
+            ->setBackgroundColor('#27ACB2')
+            ->setPaddingAll(ComponentSpacing::LG)
+            ->setPaddingBottom('20px')
+            ->setContents([$headerTextComponent]);
+    }
+
+    private function createDeadlineBubbleBody(Deadline $deadline)
+    {
+        $bodyText = CountDownMessageBuilder::new(today(), $deadline)->__toString();
+        $bodyTextComponent = TextComponentBuilder::builder()
+            ->setText($bodyText)
+            ->setSize(ComponentFontSize::SM)
+            ->setWrap(true)
+            ->setFlex(0);
+        return BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::VERTICAL)
+            ->setSpacing(ComponentSpacing::MD)
+            ->setPaddingAll(ComponentSpacing::LG)
+            ->setContents([$bodyTextComponent]);
+    }
+}
